5a0535d01d70cacde7c46799bc8d2fedcbf43d30
Juliana Tocci <142944305+julianatocci@users.noreply.github.com>
2024-03-13 17:52:08 -0300

    Add files via upload

---
+++ SequenciaFibonacci.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Fibonacci
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Informe um número inteiro: ");
+            int number = int.Parse(Console.ReadLine());
+
+            if (VerifyFibonacci(number))
+            {
+                Console.WriteLine("O número " + number + " pertence à sequência de Fibonacci.");
+            }
+            else
+            {
+                Console.WriteLine("O número " + number + " não pertence à sequência de Fibonacci.");
+            }
+            Console.ReadLine();
+        }
+
+        static bool VerifyFibonacci(int num, int a = 0, int b = 1)
+        {
+            if (num == a) 
+            {
+                return true;
+            }
+            else if (num < a)
+            {
+                return false;
+            }
+            return VerifyFibonacci(num, b, a + b);
+        }
+    }
+}
