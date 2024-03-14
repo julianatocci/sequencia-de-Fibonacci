316f78dabb8e2f3c79afeba1c9a6aa3ae5c952dd
Juliana Tocci <142944305+julianatocci@users.noreply.github.com>
2024-03-14 13:01:14 -0300

    Update SequenciaFibonacci.cs

--- SequenciaFibonacci.cs
+++ SequenciaFibonacci.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 
 namespace Fibonacci
 {
@@ -6,31 +6,28 @@ namespace Fibonacci
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Informe um número inteiro: ");
+            Console.WriteLine("Qual número inteiro você gostaria de saber se faz parte da sequência de Fibonacci?");
             int number = int.Parse(Console.ReadLine());
 
-            if (VerifyFibonacci(number))
+            int previousNumber = 0;
+            int currentNumber = 1;
+            int newNumber = previousNumber + currentNumber;
+            while (newNumber <= number)
             {
-                Console.WriteLine("O número " + number + " pertence à sequência de Fibonacci.");
+                previousNumber = currentNumber;
+                currentNumber = newNumber;
+                newNumber = previousNumber + currentNumber; 
+            }
+
+            if (number == 0 || number == 1 || number == newNumber || number == currentNumber)
+            {
+                Console.WriteLine($"O número {number} faz parte da sequência de Fibonacci.");
             }
             else
             {
-                Console.WriteLine("O número " + number + " não pertence à sequência de Fibonacci.");
+                Console.WriteLine($"O número {number} não faz parte da sequência de Fibonacci.");
             }
             Console.ReadLine();
-        }
-
-        static bool VerifyFibonacci(int num, int a = 0, int b = 1)
-        {
-            if (num == a) 
-            {
-                return true;
-            }
-            else if (num < a)
-            {
-                return false;
-            }
-            return VerifyFibonacci(num, b, a + b);
         }
     }
 }
